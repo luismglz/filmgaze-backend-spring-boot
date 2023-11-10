da0250a6819dddc00b18dc6bc77ccc948bd87f2e
Luis GL <lmgl887@gmail.com>
2023-11-10 12:58:32 -0600

    add: empty project

---
+++ src/main/java/com/ruisu/filmgazebackendspringboot/FilmgazeBackendSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.ruisu.filmgazebackendspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmgazeBackendSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmgazeBackendSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ruisu/filmgazebackendspringboot/FilmgazeBackendSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruisu.filmgazebackendspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmgazeBackendSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
