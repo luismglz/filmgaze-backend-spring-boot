0560fbaee8149aff0d45027daf78956d65484e89
Luis GL <lmgl887@gmail.com>
2023-11-10 14:14:02 -0600

    add: optional in get a movie

--- src/main/java/com/ruisu/filmgazebackendspringboot/controllers/MovieController.java
+++ src/main/java/com/ruisu/filmgazebackendspringboot/controllers/MovieController.java
@@ -2,14 +2,17 @@ package com.ruisu.filmgazebackendspringboot.controllers;
 
 import com.ruisu.filmgazebackendspringboot.entities.Movie;
 import com.ruisu.filmgazebackendspringboot.services.MovieService;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/movies")
@@ -19,8 +22,14 @@ public class MovieController {
     private MovieService movieService;
 
     @GetMapping
-    public ResponseEntity<List<Movie>> allMovies() {
+    public ResponseEntity<List<Movie>> getAllMovies() {
         return new ResponseEntity<>(movieService.allMovies(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Movie>> getMovie(@PathVariable ObjectId id){
+        return new ResponseEntity<>(movieService.getMovie(id), HttpStatus.OK);
+
     }
 
 }
