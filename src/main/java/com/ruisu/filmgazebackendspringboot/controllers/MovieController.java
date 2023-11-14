ce4badbab07c231700ad0e13dce4266175cc917c
Luis GL <lmgl887@gmail.com>
2023-11-14 00:13:12 -0600

    hotfix: enable cross origin requests

--- src/main/java/com/ruisu/filmgazebackendspringboot/controllers/MovieController.java
+++ src/main/java/com/ruisu/filmgazebackendspringboot/controllers/MovieController.java
@@ -3,16 +3,17 @@ package com.ruisu.filmgazebackendspringboot.controllers;
 import com.ruisu.filmgazebackendspringboot.entities.Movie;
 import com.ruisu.filmgazebackendspringboot.services.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
+
+
+@CrossOrigin(origins = "http://localhost:5173/")
 @RestController
 @RequestMapping("/api/v1/movies")
 public class MovieController {
